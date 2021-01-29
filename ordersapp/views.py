99ade980bac263db1d368b64b9a746d04aa1332f
ZeroXKiritsu <kalen795@gmail.com>
2021-01-30 02:00:18 +0300

    12

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.views.generic.detail import DetailView
+from django.http import JsonResponse
 
+from mainapp.models import Product
 from basketapp.models import Basket
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
@@ -141,3 +143,11 @@ def product_quantity_update_delete(instance, **kwargs):
     instance.product.quantity += instance.product.reserved
     instance.product.reserved = 0
     instance.product.save()
+
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.filter(pk=int(pk)).first()
+        if product:
+            return JsonResponse({"price": product.price})
+        else:
+            return JsonResponse({"price": 0})
